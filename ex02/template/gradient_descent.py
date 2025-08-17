0fbdb08abb693a8260644cb31c537241f7f49c9b
oraziorillo <oraziorillo@gmail.com>
2025-08-17 13:28:16 +0200

    Updated gradient descent in ex2

--- ex02/template/gradient_descent.py
+++ ex02/template/gradient_descent.py
@@ -5,6 +5,11 @@ Gradient Descent
 """
 
 
+import numpy as np
+
+from costs import compute_mse
+
+
 def compute_gradient(y, tx, w):
     """Computes the gradient at w.
 
@@ -16,11 +21,9 @@ def compute_gradient(y, tx, w):
     Returns:
         An array of shape (2, ) (same shape as w), containing the gradient of the loss at w.
     """
-    # ***************************************************
-    # INSERT YOUR CODE HERE
-    # TODO: compute gradient vector
-    # ***************************************************
-    raise NotImplementedError
+    e = y - np.dot(tx, w)
+    N = e.shape[0]
+    return - 1/N * (tx.T @ e), e 
 
 
 def gradient_descent(y, tx, initial_w, max_iters, gamma):
@@ -42,24 +45,15 @@ def gradient_descent(y, tx, initial_w, max_iters, gamma):
     losses = []
     w = initial_w
     for n_iter in range(max_iters):
-        # ***************************************************
-        # INSERT YOUR CODE HERE
-        # TODO: compute gradient and loss
-        # ***************************************************
-        raise NotImplementedError
-        # ***************************************************
-        # INSERT YOUR CODE HERE
-        # TODO: update w by gradient
-        # ***************************************************
-        raise NotImplementedError
+        grad, err = compute_gradient(y, tx, w)
+        loss = compute_mse(err)
+        w -= gamma * grad
+        # store w and loss
+        ws.append(w)
+        losses.append(loss)
 
         # store w and loss
         ws.append(w)
         losses.append(loss)
-        print(
-            "GD iter. {bi}/{ti}: loss={l}, w0={w0}, w1={w1}".format(
-                bi=n_iter, ti=max_iters - 1, l=loss, w0=w[0], w1=w[1]
-            )
-        )
 
     return losses, ws
