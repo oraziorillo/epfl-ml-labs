418626293ec2b4d483f772e2e6d27ab0308bcb75
oraziorillo <oraziorillo@gmail.com>
2025-08-17 13:02:28 +0200

    Added loss computation functions in ex2

--- ex02/template/costs.py
+++ ex02/template/costs.py
@@ -3,20 +3,22 @@
 
 import numpy as np
 
+def compute_mse(e):
+    return 1/2 * np.mean(e**2)
+    
+def compute_mae(e):
+    return np.mean(np.abs(e))
 
 def compute_loss(y, tx, w):
     """Calculate the loss using either MSE or MAE.
 
     Args:
-        y: shape=(N, )
-        tx: shape=(N,2)
-        w: shape=(2,). The vector of model parameters.
+        y: numpy array of shape=(N, )
+        tx: numpy array of shape=(N,2)
+        w: numpy array of shape=(2,). The vector of model parameters.
 
     Returns:
         the value of the loss (a scalar), corresponding to the input parameters w.
     """
-    # ***************************************************
-    # INSERT YOUR CODE HERE
-    # TODO: compute loss by MSE
-    # ***************************************************
-    raise NotImplementedError
+    e = y - np.dot(tx, w)
+    return compute_mse(e)
